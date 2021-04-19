6598b86823d7cf596bed132a36a61b5fd76a7ecb
plaprus <pawel.laprus@hyland.com>
2021-04-19 10:22:52 +0200

    Added first unit test

---
+++ Business.Tests/SerialPortParserTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Business.Tests
+{
+    [TestFixture]
+    public class SerialPortParserTests
+    {
+        [Test]
+        public void ParsePort_COM1_Returns1()
+        {
+            int result = SerialPortParser.ParsePort("COM1");
+            Assert.That(result, Is.EqualTo(1));
+
+            //older style of Asserts in NUnit
+            //Assert.AreEqual(1, result);
+        }
+    }
+}
